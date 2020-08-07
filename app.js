ea3390f4e4856ae619bd7d8928a071e80fe1c63b
Nic Meg <nicmeg54@gmail.com>
2020-08-07 11:23:52 +1000

    Add search functionality

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos');
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector('.search input');
 
-const generateTemplate = todo => {
-
+const generateTemplate = (todo) => {
   const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <span>${todo}</span>
@@ -11,26 +11,39 @@ const generateTemplate = todo => {
     `;
 
   list.innerHTML += html;
+};
 
-}
-
-addForm.addEventListener('submit', e => {
-
+addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const todo = addForm.add.value.trim();
 
-  if(todo.length){
+  if (todo.length) {
     generateTemplate(todo);
     addForm.reset();
-  } 
-
-})
+  }
+});
 
 // delete todos
-list.addEventListener('click', e => {
-
-  if(e.target.classList.contains('delete')){
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
 
-})
+const filterTodos = (term) => {
+
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+  Array.from(list.children)
+  .filter((todo) => todo.textContent.includes(term))
+  .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+// keyup event
+search.addEventListener('keyup', () => {
+  const term = search.value.trim();
+  filterTodos(term);
+
+});
